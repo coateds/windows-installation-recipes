b1fdfc9d4402a738322cefc9d4d6ae315517afa1
Dave Coate <coateds@outlook.com>
2017-10-10 15:24:35 -0700

    First cut of install-packages recipe

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # windows-installation-recipes
 
-TODO: Enter the cookbook description here.
+This is my library cookbook for Windows. It will combine code/resources from cookbooks/recipes such as:
 
+* windows-rdp
+* install-windows-packages
+* Install_Packages
+* windows_tweaks
+  * default (disable-servermanager, make PowerShell link)
+  * windows-rdp (enable rdp for my HyperV image)
+  * install-iis (includes server-info functionality)
+* windows_ad (supermarket cookbook see xp-host-int-kit for example usage)
+
+Notes on the Chocolatey cookbook
+* https://supermarket.chef.io/cookbooks/chocolatey
+* https://github.com/chocolatey/chocolatey-cookbook
+* For integration testing, look https://github.com/chocolatey/chocolatey-cookbook/blob/master/test/integration/chef_latest/default_spec.rb
+* At this time, I do not think there is a unit test for installing Chocolatey
+
+Attributes
+```
+# NOTE: attributes are case sensitive!!
+# set the following attributes to 'y' to install/upgrade
+default['install-packages']['upgrade-chocolatey'] = 'n'
+
+default['install-packages']['git']             = 'y'
+default['install-packages']['vscode']          = 'y'
+default['install-packages']['putty']           = 'n'
+default['install-packages']['curl']            = 'n'
+default['install-packages']['azstorexplorer']  = 'n'
+default['install-packages']['chefdk']          = 'n'
+default['install-packages']['sysinternals']    = 'n'
+default['install-packages']['poshgit']         = 'n'
+default['install-packages']['pester']          = 'n'
+default['install-packages']['rdcman']          = 'n'
+default['install-packages']['slack']           = 'n'
+
+# packages that require a reboot, set the requestreboot attribute to 'y' as well
+default['install-packages']['winazpowershell'] = 'y'
+default['install-packages']['requestreboot']   = 'y'
+```
---
+++ recipes/install-packages.rb
@@ -0,0 +1,91 @@
+#
+# Cookbook:: windows-installation-recipes
+# Recipe:: install-packages
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe 'chocolatey::default'
+
+# This will run an upgrade for images that include an old version
+if node['install-packages']['upgrade-chocolatey'].to_s == 'y'
+  chocolatey_package 'chocolatey' do
+    action :upgrade
+  end
+end
+
+#chocolatey_package 'visualstudiocode'
+if node['install-packages']['vscode'].to_s == 'y'
+  chocolatey_package 'visualstudiocode'
+end
+
+# git will not be available to a logged on user (logout/logon to use it)
+if node['install-packages']['git'].to_s == 'y'
+  chocolatey_package 'git' do
+     options '--params /GitAndUnixToolsOnPath'
+  end
+end
+
+if node['install-packages']['chefdk'].to_s == 'y'
+  chocolatey_package 'chefdk'
+end
+
+if node['install-packages']['putty'].to_s == 'y'
+  chocolatey_package 'putty'
+end
+if node['install-packages']['sysinternals'].to_s == 'y'
+  chocolatey_package 'sysinternals'
+end
+
+if node['install-packages']['curl'].to_s == 'y'
+  chocolatey_package 'curl'
+end
+
+# Non functional??  Seems to work now??
+if node['install-packages']['poshgit'].to_s == 'y'
+  chocolatey_package 'poshgit'
+end
+
+if node['install-packages']['pester'].to_s == 'y'
+  chocolatey_package 'pester'
+end
+
+if node['install-packages']['rdcman'].to_s == 'y'
+  chocolatey_package 'rdcman'
+end
+
+if node['install-packages']['slack'].to_s == 'y'
+  chocolatey_package 'slack'
+end
+
+if node['install-packages']['azstorexplorer'].to_s == 'y'
+  chocolatey_package 'azurestorageexplorer'
+end
+
+if node['install-packages']['winazpowershell'].to_s == 'y'
+  chocolatey_package 'windowsazurepowershell' do
+    action :install
+    # notifies :reboot_now, 'reboot[restart-computer]', :delayed
+  end
+end
+
+if node['install-packages']['powershell51'].to_s == 'y'
+  cookbook_file 'Win8.1AndW2K12R2-KB3191564-x64.msu' do
+  	source 'Win8.1AndW2K12R2-KB3191564-x64.msu'
+  end
+
+  msu_package 'Win8.1AndW2K12R2-KB3191564-x64.msu' do
+    source 'Win8.1AndW2K12R2-KB3191564-x64.msu'
+    # action :remove
+    action :install
+    # notifies :reboot_now, 'reboot[Restart Computer]', :immediately
+  end
+end
+
+if node['install-packages']['requestreboot'].to_s == 'y'
+  reboot 'restart-computer' do
+    action :request_reboot
+    ignore_failure
+  end
+end
+
+# notifies :reboot_now, 'reboot[Restart Computer]', :immediately
