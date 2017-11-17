cd7363ad50664e8608b9c5a45525edce2897e74f
Dave <you@example.com>
2017-11-17 08:13:34 -0800

    Write access-rdp tests

--- test/smoke/default/access-rdp_test.rb
+++ test/smoke/default/access-rdp_test.rb
@@ -5,14 +5,32 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+#### access-rdp ####
+# Allow Access
+describe registry_key ({
+  hive: 'HKEY_LOCAL_MACHINE',
+  key: 'SYSTEM\CurrentControlSet\Control\Terminal Server'
+}) do
+  its('fDenyTSConnections') { should eq 0 }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# NLA Only
+describe registry_key ({
+  hive: 'HKEY_LOCAL_MACHINE',
+  key: 'SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp'
+}) do
+  its('UserAuthentication') { should eq 1 }
 end
+
+# Is the firewall rule enabled for all profiles?
+fw_rule_enabled_script = <<-EOH
+  $RDFwRuleEnabled = $true
+  ForEach ($Item in Get-NetFirewallRule -DisplayGroup "Remote Desktop"){If ($Item.Enabled -eq "False") {$RDFwRuleEnabled = $false}}
+  return $RDFwRuleEnabled
+EOH
+
+# The .chop is necessary to trim the carriage return/line feed from the output
+describe powershell(fw_rule_enabled_script) do
+  its('stdout.chop') { should eq 'True' }
+end
+#### /access-rdp ####
